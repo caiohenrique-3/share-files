ad70d1745ddfbe4a40221cc7cc59ddfdefbcaf5c
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2025-01-29 19:07:03 -0300

    feat(upload/download): add file upload and download functionality

--- sf.go
+++ sf.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
+
+type Data struct {
+	ID       string `json:"id"`
+	Filename string `json:"filename"`
+}
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	log.Println("/upload/ hit")
@@ -22,13 +29,14 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	}
 	defer clientFile.Close()
 
-	log.Printf("Uploaded File: %+v\n", handler.Filename)
-	log.Printf("File Size: %+v\n", handler.Size)
+	log.Printf("Name: %+v\n", handler.Filename)
+	log.Printf("Size: %+v\n", handler.Size)
 
 	fileBytes, err := io.ReadAll(clientFile)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
 
 	dst, err := os.Create("./user-uploads/" + handler.Filename)
@@ -41,7 +49,106 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 
 	dst.Write(fileBytes)
 
-	fmt.Fprintf(w, "Successfully uploaded file\n")
+	uuid, err := exec.Command("uuidgen").Output()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Trim newline from UUID output
+	uuidStr := string(uuid)
+	uuidStr = uuidStr[:len(uuidStr)-1]
+
+	log.Println("UUID:", uuidStr)
+
+	err = checkFile("./data.json")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	jsonData := Data{uuidStr, handler.Filename}
+	file, err := os.OpenFile("data.json", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	defer file.Close()
+
+	var data []Data
+	decoder := json.NewDecoder(file)
+	decoder.Decode(&data)
+
+	data = append(data, jsonData)
+
+	file.Seek(0, 0)
+	encoder := json.NewEncoder(file)
+	encoder.Encode(data)
+
+	fmt.Fprintln(w, "Successfully uploaded file!")
+	fmt.Fprintln(w, "/download/"+uuidStr)
+}
+
+func downloadFile(w http.ResponseWriter, r *http.Request) {
+	log.Println("/download/ hit")
+
+	uuid := r.URL.Path[len("/download/"):]
+
+	var data []Data
+	file, err := os.Open("./data.json")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	decoder.Decode(&data)
+
+	for _, item := range data {
+		if item.ID == uuid {
+			filePath := "./user-uploads/" + item.Filename
+			fileToDownload, err := os.Open(filePath)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "Internal Server Error", 500)
+				return
+			}
+			defer fileToDownload.Close()
+
+			w.Header().Set("Content-Disposition", "attachment; filename="+item.Filename)
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Header().Set("Content-Length", fmt.Sprintf("%d", getSize(filePath)))
+
+			io.Copy(w, fileToDownload)
+			return
+		}
+	}
+
+	http.Error(w, "File Not Found", 404)
+}
+
+func getSize(filePath string) int64 {
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		return 0
+	}
+	return fileInfo.Size()
+}
+
+func checkFile(filename string) error {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		_, err := os.Create(filename)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func main() {
@@ -54,13 +161,26 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		log.Println("user-uploads dir created")
 	}
 
-	log.Println("user-uploads dir created")
+	_, err = os.Stat("./data.json")
+	if os.IsNotExist(err) {
+		log.Println(err)
+
+		_, err := os.Create("./data.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("data.json file created")
+	}
 
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/download/", downloadFile)
 
 	log.Println("Listening on :9090...")
 	err = http.ListenAndServe(":9090", nil)
